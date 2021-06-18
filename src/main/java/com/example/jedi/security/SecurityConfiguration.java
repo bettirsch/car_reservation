c40199b679f1cfa2d983fb839860f1a75c2001ac
Bettina Hirsch <bettina_hirsch@epam.com>
2021-06-18 09:54:20 +0200

    @value from application.properties

--- src/main/java/com/example/jedi/security/SecurityConfiguration.java
+++ src/main/java/com/example/jedi/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.jedi.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,11 +15,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Value("${login.username}")
+	private String userName;
+	
+	@Value("${login.password}")
+	private String password;
+	
+	@Value("${login.role}")
+	private String role;
+	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		auth.inMemoryAuthentication().passwordEncoder(encoder).withUser("user").password(encoder.encode("pass"))
-				.roles("ADMIN");
+		auth.inMemoryAuthentication().passwordEncoder(encoder).withUser(userName).password(encoder.encode(password))
+				.roles(role);
 	}
 
 	@Override
